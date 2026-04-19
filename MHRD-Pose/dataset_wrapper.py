bdeee3a3ff8acf4482429e2a2f38a396feecd5e7
Ammar Qammaz <ammarkov@gmail.com>
2026-04-19 22:14:53 +0300

    ...

--- MHRD-Pose/dataset_wrapper.py
+++ MHRD-Pose/dataset_wrapper.py
@@ -135,7 +135,6 @@ class DatasetHMR(OriginalDatasetHMR):
             sys.path.append('../MHR/')
             sys.path.append('../')
             from mhr.mhr import MHR
-            #from mhr_hmr import MHRHMR
             # conversion.py uses local-relative imports so its directory must be on path
             import sys as _sys, os as _os
             _proj_root = _os.path.dirname(_os.path.dirname(_os.path.abspath(__file__)))
